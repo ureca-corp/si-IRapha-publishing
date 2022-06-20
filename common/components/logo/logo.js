50045fe54995565b925de0aa37e9a79daa9de7be
NightOwl <steelman2019e@gmail.com>
2022-06-20 11:39:46 +0900

    feat: Logo 영역 구현

--- common/components/logo/logo.js
+++ common/components/logo/logo.js
@@ -1,8 +1,4 @@
-const $logo = document.querySelector(".irm-logo__logo");
 const $maximizeBtn = document.querySelector(".irm-logo__maximize-icon-wrapper");
-const $networkStatus = document.querySelector(
-  ".irm-logo__network-status-wrapper"
-);
 
 // 최대화/최소화 토글
 const toggleFullScreen = () => {
@@ -11,10 +7,5 @@ const toggleFullScreen = () => {
 
   if (document.exitFullscreen) return document.exitFullscreen();
 };
-
-// Events
-$logo.addEventListener("click", () => {
-  alert("logo click");
-});
 
 $maximizeBtn.addEventListener("click", toggleFullScreen);
